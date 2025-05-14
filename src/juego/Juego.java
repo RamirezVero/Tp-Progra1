ba95b176333e6b2fdebb07305478bc7907bba587
Veronica <verdani8@gmail.com>
2025-05-14 15:43:20 -0300

    fondo provisorio

---
+++ src/juego/Juego.java
@@ -0,0 +1,52 @@
+package juego;
+
+
+import java.awt.Color;
+import java.awt.Image;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+import entorno.InterfaceJuego;
+
+public class Juego extends InterfaceJuego
+{
+	// El objeto Entorno que controla el tiempo y otros
+	private Entorno entorno;
+	
+	// Variables y métodos propios de cada grupo
+	private Image fondo;
+	
+	Juego()
+	{
+		// Inicializa el objeto entorno
+		this.entorno = new Entorno(this, "El camino de Gondolf", 800, 600);
+		
+		
+		// Inicializar lo que haga falta para el juego
+		this.fondo = Herramientas.cargarImagen("suelo.jpg");
+		this.entorno.dibujarImagen(fondo, 400, 300, 0);
+
+		// Inicia el juego!
+		this.entorno.iniciar();
+	}
+
+	/**
+	 * Durante el juego, el método tick() será ejecutado en cada instante y 
+	 * por lo tanto es el método más importante de esta clase. Aquí se debe 
+	 * actualizar el estado interno del juego para simular el paso del tiempo 
+	 * (ver el enunciado del TP para mayor detalle).
+	 */
+	public void tick()
+	{
+		// Procesamiento de un instante de tiempo
+		this.entorno.dibujarImagen(fondo, 400, 300, 0);
+		
+	}
+	
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args)
+	{
+		Juego juego = new Juego();
+	}
+}
